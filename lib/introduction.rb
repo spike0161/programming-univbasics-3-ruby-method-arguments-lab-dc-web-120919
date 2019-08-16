fccfeb046708900cf77c4630fadecc99b30355c3
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-16 16:49:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,13 @@
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
+
+
+
+
+
+
+
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
